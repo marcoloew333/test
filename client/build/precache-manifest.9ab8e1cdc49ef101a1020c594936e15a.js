5d5da4e8836457187017fc2fb0e28202645a650d
Marco <30937581+marcoloew333@users.noreply.github.com>
2019-08-03 21:08:36 +0200

    First Commit

---
+++ client/build/precache-manifest.9ab8e1cdc49ef101a1020c594936e15a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b37ec8f8ba3c07ac7395",
+    "url": "/static/js/main.b37ec8f8.chunk.js"
+  },
+  {
+    "revision": "7382dbe11836f5c58744",
+    "url": "/static/js/1.7382dbe1.chunk.js"
+  },
+  {
+    "revision": "b37ec8f8ba3c07ac7395",
+    "url": "/static/css/main.9b358d00.chunk.css"
+  },
+  {
+    "revision": "ebabd40f33d5b46883f798b1bcead993",
+    "url": "/index.html"
+  }
+];
